04b9ced05b6594c10c9ff9adc3d4b5afde9d0582
chengang <chengang01@live.com>
2023-08-25 09:07:15 +0800

    SyncCall2 & SyncCallServer2 surport dynamic param

--- src/Framework/WebServer/Decorator/SyncCall.ts
+++ src/Framework/WebServer/Decorator/SyncCall.ts
@@ -18,3 +18,28 @@ export function SyncCall(target: any, propertyName: string, descriptor: TypedPro
         return ret
     }
 }
+
+/**
+ * 异步函数变为同步函数，当前进程有效
+ * @param param_index 动态参数的索引
+ */
+export function SyncCall2(param_index?:number)
+{
+    return (target: any, propertyName: string, descriptor: TypedPropertyDescriptor<Function>)=>
+    {
+        let method = descriptor.value
+        descriptor.value = function () 
+        {
+            let key = propertyName
+            if(param_index != undefined&&param_index<arguments.length)
+            {
+                key = propertyName + "_" + arguments[param_index]
+            }
+            let ret = GSyncQueueTool.add(key,async ()=>
+            {
+                return await method.apply(target, arguments)
+            })
+            return ret
+        }
+    }
+}
--- src/Framework/WebServer/Decorator/SyncCallServer.ts
+++ src/Framework/WebServer/Decorator/SyncCallServer.ts
@@ -8,7 +8,45 @@ import { GMongoCacheSer } from "../../Service/MongoCacheService"
  * 只支持mongo模式
  * @returns 
  */
-export let SyncCallServer=function()
+export function SyncCallServer(target: any, propertyName: string, descriptor: TypedPropertyDescriptor<Function>) 
+{
+    let method = descriptor.value
+    descriptor.value = async ()=>
+    {
+        let key = "sync_"+method.name
+        let func = async ()=>
+        {
+            let item = await GMongoCacheSer.getData(key)
+            let ret = null
+            while(item)
+            {
+                await core.sleep(200)
+                item = await GMongoCacheSer.getData(key)
+            }
+            //10秒后过期，避免卡死
+            let mcm = await GMongoCacheSer.addData(key,true,Date.now()+10*1000)
+            if(!mcm)
+            {
+                await func()
+            }
+            return
+        }
+        await func()
+        let ret = GSyncQueueTool.add(method.name,async ()=>
+        {
+            return await method.apply(self, arguments)
+        })
+        await GMongoCacheSer.deleteOne({key})
+        return ret
+    }
+}
+/**
+ * 异步函数变为同步函数
+ * 服务器间的异步，效率低
+ * 只支持mongo模式
+ * @returns 
+ */
+export let SyncCallServer2=function(params_index?:number)
 {
     return function(target: any, propertyName: string, descriptor: TypedPropertyDescriptor<Function>) 
     {
@@ -16,6 +54,10 @@ export let SyncCallServer=function()
         descriptor.value = async ()=>
         {
             let key = "sync_"+method.name
+            if(params_index != undefined&&params_index<arguments.length)
+            {
+                key = key+"_"+arguments[params_index]
+            }
             let func = async ()=>
             {
                 let item = await GMongoCacheSer.getData(key)
